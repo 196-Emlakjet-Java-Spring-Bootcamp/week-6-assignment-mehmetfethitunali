69742761b034ec58f590dd962d1a81bd0fa64537
tunalime <mfethi96@gmail.com>
2022-06-27 19:49:09 +0300

    first commit.

---
+++ src/main/java/com/example/week6/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.week6.dao;
+import com.example.week6.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+
+    @Query(value = "select * from users order by random() limit 1", nativeQuery = true)
+    Users getRandomUser();
+}
---
+++ src/main/java/com/example/week6/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.example.week6.dto;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class UserDto {
+
+    private String userName;
+
+    private String userSurname;
+
+    private String userMail;
+
+}
